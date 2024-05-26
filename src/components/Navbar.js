bf6db0c6b40082a46e4c4576110073abea439753
Govind Patel <govind.patel@hostbooks.com>
2024-05-26 09:59:56 +0530

    thiems color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,22 +13,28 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarScroll">
           <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
               {/* <a className="nav-link active" aria-current="page" href="">Home</a> */}
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
           </ul>
+          <div className="btn btn-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="btn btn-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="btn btn-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="btn btn-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="btn btn-light rounded mx-2" onClick={()=>{props.toggleMode('light')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+          <div className="btn btn-dark rounded mx-2" onClick={()=>{props.toggleMode('dark')}} style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
           {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
             <button className="btn btn-primary" type="submit">Search</button>
             <button className="btn btn-outline-success" type="submit">Search</button>
           </form> */}
-            <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
-                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
-            </div>
+            {/* <div className={`form-check form-switch text-${props.mode === 'light'?'dark':'light'}`}>
+                <input className="form-check-input" onClick={()=>{props.toggleMode(null)}} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
+            </div> */}
         </div>
       </div>
     </nav>
